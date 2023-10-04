c9ad27e811cefdbc6b4b32012dd05209c0943254
kelvin-Mudzingwa <139792230+kelvin-Mudzingwa@users.noreply.github.com>
2023-10-04 13:26:44 +0200

    Add project files.

---
+++ LibraryCatalog.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+using System.Security.Principal;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using static System.Console;
+
+namespace Library
+{
+    internal class LibraryCatalog
+    {
+        ArrayList BookList;
+        int sortedState = 0;
+
+        public LibraryCatalog()
+        {
+            BookList = new ArrayList();
+        }
+        public void ReadData()
+        {
+            StreamReader sr = new StreamReader("BookData.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] fildes = line.Split(',');
+                int isbn = int.Parse(fildes[0].Trim());
+                String title = fildes[1].Trim();
+                String author = fildes[2].Trim();
+                int publicationYear = int.Parse(fildes[3].Trim());
+
+                Book book = new Book(isbn, title, author, publicationYear);
+                BookList.Add(book);
+            }
+            sr.Close();
+        }
+        public void Add(Book book)
+        {
+            BookList.Add(book);
+            sortedState = 0;
+        }
+        public void Delete(int wanted)
+        {
+            //using the binary search to find the book
+            int pos = Find(wanted);
+            BookList.RemoveAt(pos);
+        }
+        private void swap(int x, int y)
+        {
+            object temp = BookList[x];
+            BookList[x] = BookList[y];
+            BookList[y] = temp;
+        }
+        /// <summary>
+        /// check this out why it was not running 
+        /// </summary>
+        private void bubleSort()
+        {
+            for (int index = 0; index < BookList.Count-1; index++)
+            {
+                for (int i = 0; i < BookList.Count-1; i++)
+                {
+                    //casting 
+                    Book book1 = (Book)BookList[i];
+                    Book book2 = (Book)BookList[i+1];
+                    //the algorithim
+                    if (book1.getISBN() > book2.getISBN())
+                    {
+                        swap(i , i+1);
+                    }
+                }
+            }
+           
+        }
+        
+        private void SelectionSort()
+        {
+            int Minindex = 0;
+            for (int index = 1; index < BookList.Count; index++)
+            {
+                for (int x = index; x < BookList.Count; x++)
+                {
+                    //sorting by the author
+                    Book author1 = (Book)BookList[x];
+                    Book author2 = (Book)BookList[Minindex];
+
+                    if (author1.getAuthor().CompareTo(author2.getAuthor())<0)
+                    
+                        Minindex = x;
+                }
+                swap( index - 1,Minindex);
+            }
+           
+        }
+        private void InsertionSorted()
+        {
+            for (int pass = 1; pass < BookList.Count; pass++)
+            {
+                Book key = (Book)BookList[pass];
+                int curPos= pass - 1;
+                Book currentOne= (Book)BookList[curPos];
+                while ((curPos!=-1)&&(key.getPublicationYear()<currentOne.getPublicationYear()))
+                {
+                    curPos--;
+                    if(curPos!=-1)
+                    
+                        currentOne = (Book)BookList[curPos];
+                    
+                }
+                BookList.RemoveAt(pass);
+                BookList.Insert(curPos+ 1, key);
+            }
+        }
+        private  int BinarySearch(int key)
+        {
+            int stop = BookList.Count;
+            int start = 0;
+            
+            while(start <=stop)
+            {
+                int middle = (stop + start) / 2;
+                Book book = (Book)BookList[middle];
+                if (book.getISBN() == key)
+                {
+                    return middle;
+                }
+                else if (book.getISBN()<key)
+                {
+                    stop = middle - 1;
+                }
+                else
+                    stop = middle + 1;
+            }
+            return -1;
+        }
+        private int LinearSearch(int wanted)
+        {
+            //searching using book title
+            for(int i=0;i<BookList.Count;i++)
+            {
+                Book book = (Book)BookList[i];
+                if(book.getISBN().Equals(wanted))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public int Find(int wanted)
+        {
+            if (sortedState == 1 || sortedState == 3)
+            {
+                return BinarySearch(wanted);
+            }
+            else
+                return LinearSearch(wanted);
+        }
+
+        public void DisplayBooks()
+        {
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                Book book = (Book)BookList[i];
+                book.DisplayBook();
+            }
+        }
+        public void DisplayBook(int wanted)
+        {
+            int pos = LinearSearch(wanted);
+            Book book = (Book)BookList[pos];
+            book.DisplayBook();
+        }
+        public void INSBSort()
+        {
+            bubleSort();
+            DisplayBooks();
+            sortedState = 1;
+        }
+        public void AuthorSort()
+        {
+            SelectionSort();
+            DisplayBooks();
+            sortedState = 2;
+        }
+        public void PublishedYear()
+        {
+            InsertionSorted();
+            DisplayBooks();
+            sortedState = 3;
+        }
+        public void WriteData()
+        {
+            StreamWriter wr = new StreamWriter("Output.txt");
+            for(int i = 0;i < BookList.Count;i++)
+            {
+                Book book = (Book)BookList[i];  
+                WriteLine(book.getISBN()+","+book.getTitle()+","+book.getAuthor()+","+book.getPublicationYear());
+            }
+            wr.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryCatalog catalogue = new LibraryCatalog();
+            catalogue.ReadData();
+            DisplayOptions();
+            int choice = int.Parse(ReadLine());
+            ProcessOption(catalogue, choice);
+            while (choice != 8)
+            {
+                DisplayOptions();
+                choice = int.Parse(ReadLine());
+                ProcessOption(catalogue, choice);
+            }
+            ReadLine();
+        }
+
+        static void DisplayOptions()
+        {
+            WriteLine("Choose one of the following options: ");
+            WriteLine("1. Add a new book");
+            WriteLine("2. Delete a book");
+            WriteLine("3. Display book details");
+            WriteLine("4. List all books");
+            WriteLine("5. Sort catalogue in ascending order of ISBN number, then display list");
+            WriteLine("6. Sort catalogue in ascending order of Author then display list");
+            WriteLine("7. Sort catalogue in ascending order of publication year, then display list");
+            WriteLine("8. Save data and quit");
+            Write("Choice: ");
+        }
+
+        static void ProcessOption(LibraryCatalog catalogue, int choice)
+        {
+            WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    AddBook(catalogue);
+                    WriteLine();
+                    break;
+                case 2:
+                    DeleteBook(catalogue);
+                    WriteLine();
+                    break;
+                case 3:
+                    DisplayBookDetail(catalogue);
+                    WriteLine();
+                    break;
+                case 4:
+                    catalogue.DisplayBooks();
+                    WriteLine();
+                    break;
+                case 5:
+                    catalogue.INSBSort();
+                    WriteLine();
+                    break;
+                case 6:
+                    catalogue.AuthorSort();
+                    WriteLine();
+                    break;
+                case 7:
+                    catalogue.PublishedYear();
+                    WriteLine();
+                    break;
+                default:
+                    catalogue.WriteData();
+                    WriteLine("Goodbye, the data will now be written to the text file");
+                    break;
+            }
+        }
+        static void AddBook(LibraryCatalog catalogue)
+        {
+            Write("Enter ISBN number: ");
+            int isbn = int.Parse(ReadLine());
+            Write("Enter Book Titile: ");
+            string title=ReadLine();
+            Write("Enter Author: ");
+            string author = ReadLine();
+            Write("Enter year published: ");
+            int year=int.Parse(ReadLine());
+
+            Book book=new Book(isbn, title, author,year);
+            catalogue.Add(book);
+        }
+
+        static void DeleteBook(LibraryCatalog catalogue)
+        {
+            Write("Enter the ISBN number: ");
+            int isbn = int.Parse(ReadLine());
+            int pos = catalogue.Find(isbn);
+            if(pos == -1)
+            {
+                Write("Not Found!");
+            }
+            else
+            {
+                catalogue.Delete(isbn);
+                WriteLine("Book deleted.");
+            }
+           
+        }
+        static void DisplayBookDetail(LibraryCatalog catalogue)
+        {
+            Write("Enter the ISBN number ");
+            int isbn = int.Parse(ReadLine());
+            int pos =catalogue.Find(isbn);
+            if(pos == -1)
+            {
+                WriteLine("not found!");
+            }
+            else
+            {
+                catalogue.DisplayBook(isbn);
+            }
+         
+        }
+    }
+}
